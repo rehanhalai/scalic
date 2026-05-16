e39c4dd95012a90d30b53f3e656f52767094521b
Rehan Halai <halairehan01@gmail.com>
2026-05-17 00:34:59 +0530

    scaffold a react project files one by one

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <>
+      <div className="flex h-screen w-screen items-center justify-center">
+        <h1>Hello Scalic</h1>
+      </div>
+    </>
+  );
+}
